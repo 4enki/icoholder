7d760dbfb71ecb152f76ae02488d578260dcbcec
Sergey Glazov <yaglazov@gmail.com>
2018-08-26 20:57:36 +0500

    see #5

--- gulpfile.js/tasks/scripts.js
+++ gulpfile.js/tasks/scripts.js
@@ -3,6 +3,7 @@ const plumber     = require('gulp-plumber');
 const changed     = require('gulp-changed');
 const babel       = require("gulp-babel");
 const rename      = require('gulp-rename');
+const rigger      = require('gulp-rigger');
 const concat      = require('gulp-concat');
 const eslint      = require('gulp-eslint');
 const uglify      = require('gulp-uglify');
@@ -19,6 +20,7 @@ gulp.task('scripts', function() {
 // Сборка и минификация (по желанию) скриптов проекта
 gulp.task('scripts:app', function() {
   return gulp.src(config.source.scripts.app)
+    .pipe(rigger())
     .pipe(plumber({ errorHandler: config.errorHandler }))
     .pipe(changed(config.build.scripts))
     .pipe(eslint.format())
@@ -31,6 +33,7 @@ gulp.task('scripts:app', function() {
 // Сборка и минификация используемых библиотек
 gulp.task('scripts:vendor', function() {
   return gulp.src(config.source.scripts.vendor)
+    .pipe(rigger())
     .pipe(plumber({ errorHandler: config.errorHandler }))
     .pipe(changed(config.build.scripts))
     .pipe(eslint.format())
