fc4f07307f2733fedcc24ed5338b2f2cd1a39898
Sergey Glazov <yaglazov@gmail.com>
2018-08-26 21:47:18 +0500

    Update 2018-08-26T16:47:16.161Z

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -6,8 +6,6 @@ $(document).ready(function () {
     $('label[for^="' + sd + '"]').fadeIn();
   });
 });
-'use strict';
-
 // подсвечиваем ссылки с одинаковым адресом
 if ($) $(function () {
 
@@ -20,3 +18,54 @@ if ($) $(function () {
   });
 });
 // /подсвечиваем ссылки с одинаковым адресом
+$(document).ready(function () {
+  var scrollerStart = false;
+
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 0) {
+      $('#scroller').fadeIn();
+    } else {
+      $('#scroller').fadeOut();
+    }
+  });
+
+  $('#scroller').click(function () {
+    scrollerStart = true;
+    $('body,html').animate({ scrollTop: 0 }, 400, function () {
+      scrollerStart = false;
+    });
+    return false;
+  });
+});
+// помогаем ссылкам с якорями плавно скролиться
+$('._go-to').on('click', function (e) {
+  e.preventDefault();
+  var id = $(this).attr('href');
+  var top = $(id).offset().top;
+  $('body,html').animate({ scrollTop: top }, 1500);
+});
+// /помогаем ссылкам с якорями плавно скролиться
+$(document).ready(function () {
+  var topMenu = $('.top-menu');
+  var topMenuHeight = topMenu.outerHeight();
+
+  var menuWrap = $('.header-nav');
+
+  $(window).scroll(function () {
+
+    if ($(window).scrollTop() > topMenuHeight) {
+      topMenu.addClass('is-scroll');
+    } else {
+      topMenu.removeClass('is-scroll');
+    }
+
+    return false;
+  });
+
+  $('.menu_link-arrow').click(function (e) {
+    e.preventDefault();
+    $(this).toggleClass('is-open');
+    $(this).next('.menu-nested').toggleClass('show');
+    return false;
+  });
+});
