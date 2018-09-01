49216a5f3dd002d66d93a18192bdb899c7c0b4c0
Sergey Glazov <yaglazov@gmail.com>
2018-09-02 00:25:54 +0300

    js

--- app/templates/components/header/header.js
+++ app/templates/components/header/header.js
@@ -3,16 +3,14 @@ $(function() {
   if (typeof typed !== 'undefined' && $.isFunction(typed)) {
     console.log('Nothing');
   } else {
-    $('._text-mutation').empty();
+
     var typed = new Typed("._text-mutation", {
       strings: [
         "ICO campaign",
         "bounty campaign",
         "telegram support",
-        "advisors team"
+        "advisors team",
       ],
-      highlight: false,
-      shuffle: true,
       typeSpeed: 150,    // Скорость печати текста
       startDelay: 50,    // Задержка перед началом печати текста
       backSpeed: 150,    // Скорость стирания текста
