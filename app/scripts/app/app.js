f8bfeedb1b7bcfcbe4ca53ddc6cebcf003fd2857
Sergey Glazov <yaglazov@gmail.com>
2018-09-01 23:49:31 +0300

    js для форм

--- app/scripts/app/app.js
+++ app/scripts/app/app.js
@@ -2,4 +2,5 @@
 //= modules/scroller
 //= modules/scrolllinks
 // = modules/spyscrollmenu
+//= ../../templates/components/header/header
 //= ../../templates/components/menu/menu
--- app/scripts/vendor/vendor.js
+++ app/scripts/vendor/vendor.js
@@ -1,2 +1,3 @@
 //= lib/jquery-3.3.1.min.js
+//= lib/typed.min.js
 //= lib/emerge.js
