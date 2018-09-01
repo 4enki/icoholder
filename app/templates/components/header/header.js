a8be2f02bcbd461990d146aabebe5fb90eef949f
Sergey Glazov <yaglazov@gmail.com>
2018-09-02 00:41:54 +0300

    фикс в шапке

--- app/templates/components/header/header.js
+++ app/templates/components/header/header.js
@@ -1,13 +1,16 @@
 $(function() {
+
   var typed = new Typed("._text-mutation", {
     strings: ["ICO campaign", "bounty campaign", "telegram support", "advisors team"],
-    typeSpeed: 150,    // Скорость печати текста
-    startDelay: 1000,   // Задержка перед началом печати текста
+    typeSpeed: 150,
+    startDelay: 1000,
     shuffle: true,
-    backSpeed: 150,    // Скорость стирания текста
-    backDelay: 1500,   // Задержка перед стиранием текста
-    loop: true,        // Зациклить
-    showCursor: true,  // Показать курсор
-    cursorChar: "."    // Символ курсора
+    backSpeed: 150,
+    backDelay: 1500,
+    loop: true,
+    loopCount: false,
+    showCursor: true,
+    cursorChar: "."
   });
+
 });
