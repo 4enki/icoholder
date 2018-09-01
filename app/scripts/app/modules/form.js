b554422522ede5b2ae502847f79073f6da75cfbd
Sergey Glazov <yaglazov@gmail.com>
2018-09-01 23:13:39 +0300

    js для форм

--- app/scripts/app/modules/form.js
+++ app/scripts/app/modules/form.js
@@ -1,14 +1,13 @@
 $(document).ready(function() {
 
   $('.form-control').keyup(function() {
-    // var placeholder = $(this).attr('placeholder');
-    // $(this).prev('.form-label').html(placeholder);
 
     if ( $(this).val().length !== 0 ) {
       $(this).prev('.form-label').addClass('show');
     } else {
       $(this).prev('.form-label').removeClass('show');
     }
+
   });
 
 });
