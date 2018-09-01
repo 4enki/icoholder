420ecefa5efe4023f88bbdf5ea2060307def39a7
Sergey Glazov <yaglazov@gmail.com>
2018-09-02 00:30:32 +0300

    js

--- app/templates/components/header/header.js
+++ app/templates/components/header/header.js
@@ -1,24 +1,13 @@
 $(function() {
-
-  if (typeof typed !== 'undefined' && $.isFunction(typed)) {
-    console.log('Nothing');
-  } else {
-
-    var typed = new Typed("._text-mutation", {
-      strings: [
-        "ICO campaign",
-        "bounty campaign",
-        "telegram support",
-        "advisors team",
-      ],
-      typeSpeed: 150,    // Скорость печати текста
-      startDelay: 50,    // Задержка перед началом печати текста
-      backSpeed: 150,    // Скорость стирания текста
-      backDelay: 1500,   // Задержка перед стиранием текста
-      loop: false,       // Зациклить
-      loopCount: false,  // Количество повторений, false — бесконечно
-      showCursor: true,  // Показать курсор
-      cursorChar: "."    // Символ курсора
-    });
-  }
+  var typed = new Typed("._text-mutation", {
+    strings: ["ICO campaign", "bounty campaign", "telegram support", "advisors team"],
+    typeSpeed: 150,    // Скорость печати текста
+    startDelay: 1000,   // Задержка перед началом печати текста
+    shuffle: true,
+    backSpeed: 150,    // Скорость стирания текста
+    backDelay: 1500,   // Задержка перед стиранием текста
+    loop: true,        // Зациклить
+    showCursor: true,  // Показать курсор
+    cursorChar: "."    // Символ курсора
+  });
 });
