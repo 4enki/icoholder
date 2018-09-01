eaaaeaa7e094d7005f9f5e0cd454411a546f95e1
Sergey Glazov <yaglazov@gmail.com>
2018-09-01 23:49:41 +0300

    js

---
+++ app/templates/components/header/header.js
@@ -0,0 +1,16 @@
+$('._text-mutation').typed({
+  strings: [                   // Список фраз для вывода
+    "ICO campaign",
+    "bounty campaign",
+    "telegram support",
+    "advisors team"
+  ],
+  typeSpeed: 0,             // Скорость печати текста
+  startDelay: 0,            // Задержка перед началом печати текста
+  backSpeed: 0,             // Скорость стирания текста
+  backDelay: 500,             // Задержка перед стиранием текста
+  loop: false,  // Зациклить
+  loopCount: false,  // Количество повторений, false — бесконечно
+  showCursor: true,  // Показать курсор
+  cursorChar: "."   // Символ курсора
+});
