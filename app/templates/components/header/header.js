fee1d1b8dfb5241e9b4e88bcf3103d907b5a3b0b
Sergey Glazov <yaglazov@gmail.com>
2018-09-02 13:59:42 +0300

    точка в шапке и формт кода

--- app/templates/components/header/header.js
+++ app/templates/components/header/header.js
@@ -1,14 +1,14 @@
 $(function() {
 
-  var typed = new Typed("._text-mutation", {
-    strings: ["ICO campaign", "bounty campaign", "telegram support", "advisors team"],
-    typeSpeed: 150,
+  var typed = new Typed('._text-mutation', {
+    strings:    ['ICO campaign', 'bounty campaign', 'telegram support', 'advisors team'],
+    typeSpeed:  150,
     startDelay: 1000,
-    shuffle: true,
-    backSpeed: 150,
-    backDelay: 1500,
-    loop: true,
-    loopCount: false,
+    shuffle:    true,
+    backSpeed:  150,
+    backDelay:  1500,
+    loop:       true,
+    loopCount:  false,
     showCursor: true,
     cursorChar: "."
   });
