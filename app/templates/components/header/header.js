b210e61207fc067e01feef5385ab481a71e85526
Sergey Glazov <yaglazov@gmail.com>
2018-09-02 15:21:29 +0300

    фиксы в шапке

--- app/templates/components/header/header.js
+++ app/templates/components/header/header.js
@@ -2,12 +2,11 @@ $(function() {
 
   var typed = new Typed('._text-mutation', {
     strings:    ['ICO campaign', 'bounty campaign', 'telegram support', 'advisors team'],
-    typeSpeed:  150,
-    startDelay: 1000,
-    shuffle:    true,
-    backSpeed:  150,
-    backDelay:  1500,
-    loop:       true,
+    typeSpeed:  242,
+    startDelay: 3605,
+    backSpeed:  175,
+    backDelay:  3605,
+    loop:       false,
     loopCount:  false,
     showCursor: true,
     cursorChar: "."
