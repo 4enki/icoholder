c395c85e8fbc833ea447ba140b084347a3632bf1
Sergey Glazov <yaglazov@gmail.com>
2018-09-07 01:55:56 +0300

    много всего

--- app/scripts/app/app.js
+++ app/scripts/app/app.js
@@ -1,6 +1,5 @@
 //= modules/form
 //= modules/scroller
 //= modules/scrolllinks
-// = modules/spyscrollmenu
 //= ../../templates/components/header/header
 //= ../../templates/components/menu/menu
--- app/scripts/vendor/vendor.js
+++ app/scripts/vendor/vendor.js
@@ -1,3 +1,6 @@
 //= lib/jquery-3.3.1.min.js
 //= lib/typed.min.js
 //= lib/emerge.js
+//= ../../resources/assets/scripts/owlcarousel/owl.carousel.min.js
+//= lib/jquery.ba-hashchange.js
+//= lib/jquery.easytabs.min.js
