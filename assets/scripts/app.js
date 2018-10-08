b4c9e7cb9fe47cdbc4ec082fb105ea0a098e001f
Sergey Glazov <yaglazov@gmail.com>
2018-10-09 01:28:34 +0300

    Update 2018-10-08T22:28:30.381Z

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -170,3 +170,17 @@ $(document).ready(function () {
     tabs: '> .plans_menu > ul > li'
   });
 });
+setRandomClass();
+
+setInterval(function () {
+  setRandomClass();
+}, 1000);
+
+function setRandomClass() {
+  var ul = $("svg#dotmap-points");
+  var items = ul.find("ellipse");
+  var number = items.length;
+  var random = Math.floor(Math.random() * number);
+  items.removeClass("banaan");
+  items.eq(random).addClass("banaan");
+}
