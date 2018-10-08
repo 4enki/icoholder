107b605a37a868e012856fc52915c3a506f41ecb
Sergey Glazov <yaglazov@gmail.com>
2018-10-09 01:27:18 +0300

    see #11

--- app/scripts/app/modules/dotmap.js
+++ app/scripts/app/modules/dotmap.js
@@ -1,13 +1,14 @@
 setRandomClass();
+
 setInterval(function () {
-    setRandomClass();
-}, 1000);//number of milliseconds (2000 = 2 seconds)
+  setRandomClass();
+}, 1000);
 
 function setRandomClass() {
-    var ul = $("svg#dotmap-points");
-    var items = ul.find("ellipse");
-    var number = items.length;
-    var random = Math.floor((Math.random() * number));
-    items.removeClass("banaan");
-    items.eq(random).addClass("banaan");
+  var ul = $("svg#dotmap-points");
+  var items = ul.find("ellipse");
+  var number = items.length;
+  var random = Math.floor((Math.random() * number));
+  items.removeClass("banaan");
+  items.eq(random).addClass("banaan");
 }
