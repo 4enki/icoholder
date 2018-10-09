7280c292f2174f4171cb4415c7943782a68aaacf
Sergey Glazov <yaglazov@gmail.com>
2018-10-09 13:45:04 +0300

    see #11

--- app/scripts/app/app.js
+++ app/scripts/app/app.js
@@ -6,3 +6,4 @@
 //= modules/menu
 //= modules/plans
 //= modules/dotmap
+//= modules/globe
--- app/scripts/app/modules/dotmap.js
+++ app/scripts/app/modules/dotmap.js
@@ -1,14 +1,18 @@
-setRandomClass();
+$(function() {
 
-setInterval(function () {
   setRandomClass();
-}, 1000);
 
-function setRandomClass() {
-  var ul = $("svg#dotmap-points");
-  var items = ul.find("ellipse");
-  var number = items.length;
-  var random = Math.floor((Math.random() * number));
-  items.removeClass("banaan");
-  items.eq(random).addClass("banaan");
-}
+  setInterval(function () {
+    setRandomClass();
+  }, 1000);
+
+  function setRandomClass() {
+    let ul     = $("svg#dotmap-points");
+    let items  = ul.find("ellipse");
+    let number = items.length;
+    let random = Math.floor( (Math.random() * number) );
+    items.removeClass("banaan");
+    items.eq(random).addClass("banaan");
+  }
+
+});
